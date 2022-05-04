278bde78f5e4ae620e46d60400311488e7afce23
Dikgang29 <dcmokonyama29@gmail.com>
2022-05-04 05:47:38 +0200

    second commit to the greet function after a few modifications

--- greet.js
+++ greet.js
@@ -1,7 +1,7 @@
 function greet(firstName){
-    if(firstName=='') return 'Error, empty string';
-    else if( firstName>=0 || firstName<=0) return 'Error, cannot pass a number as a name!';
-    else if(firstName == undefined) return 'Error, have to pass a name as a string';
+    if(firstName=='') return 'ERROR, empty string';
+    else if( firstName>=0 || firstName<=0) return 'ERROR, cannot pass a number as a name!';
+    else if(firstName == undefined) return 'ERROR, have to pass a name as a string';
     
     return 'Hello, '+firstName;
   }
--- test/greet.tests.js
+++ test/greet.tests.js
@@ -5,14 +5,14 @@ describe('greet function' , function(){
     });
 
     it("should return an Error message is a number is passed as a name"  , function(){
-        assert.equal('Error, cannot pass a number as a name!',greet(10),'would work for both positive and negative numbers');
+        assert.equal('ERROR, cannot pass a number as a name!',greet(10),'have to pass a name not a number');
     });
     it("should return an Error if there is an empty string"  , function(){
-        assert.equal('Error, empty string',greet(''));  
+        assert.equal('ERROR, empty string',greet(''),'have to pass a name in the string for the function to work');  
     });
 
     it("should return an Error if nothing is passed as an argument in the function "  , function(){
-        assert.equal('Error, have to pass a name as a string',greet(),'we would have undefined fif nothing is passed in the function');  
+        assert.equal('ERROR, have to pass a name as a string',greet(),'we would have undefined if nothing is passed in the function');  
     });
 
 
